4e3b169c117d02aeeaf3862954e23af81567259c
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-16 13:42:52 +0100

    Align spacing in `webserver_public_ip` output block for consistency in Terraform configuration formatting

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "webserver_public_ip" {
   description = "Public IP of the web server"
-  value = aws_instance.cinevisions_web_server.public_ip
+  value       = aws_instance.cinevisions_web_server.public_ip
 }
 
 output "webserver_public_dns" {
