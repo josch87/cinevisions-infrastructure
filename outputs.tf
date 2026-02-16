f22abc9bfabfbe2104ed5637c4cd5377bf517267
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-16 15:13:45 +0100

    Add outputs for VPC ID and update webserver DNS output with descriptions in Terraform configuration

--- outputs.tf
+++ outputs.tf
@@ -4,5 +4,11 @@ output "webserver_public_ip" {
 }
 
 output "webserver_public_dns" {
+  description = "Public DNS of the web server"
   value = aws_instance.cinevisions_web_server.public_dns
+}
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value = aws_vpc.cinevisions_vpc.id
 }
