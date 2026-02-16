9e5e336826542e040b08b5a1c11d5ac1676b96cf
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-16 11:23:12 +0100

    Remove `map_public_ip_on_launch` from private subnets in Terraform configuration

--- main.tf
+++ main.tf
@@ -23,7 +23,6 @@ resource "aws_subnet" "cinevisions_private_subnet_1" {
   vpc_id                  = aws_vpc.cinevisions_vpc.id
   cidr_block              = var.cinevisions_private_subnet_1_cidr
   availability_zone       = var.aws_availability_zones["az1"]
-  map_public_ip_on_launch = true
   tags = {
     Name        = "cinevisions-private-subnet-1"
     Environment = var.environment
@@ -45,7 +44,6 @@ resource "aws_subnet" "cinevisions_private_subnet_2" {
   vpc_id                  = aws_vpc.cinevisions_vpc.id
   cidr_block              = var.cinevisions_private_subnet_2_cidr
   availability_zone       = var.aws_availability_zones["az2"]
-  map_public_ip_on_launch = true
   tags = {
     Name        = "cinevisions-private-subnet-2"
     Environment = var.environment
