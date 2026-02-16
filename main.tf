92712138da262425e46354835d01459bf9401205
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-16 11:30:25 +0100

    Fix CIDR block typo in security group egress rule in Terraform configuration

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ resource "aws_vpc_security_group_ingress_rule" "webserver_sg_ingress_http" {
 resource "aws_vpc_security_group_egress_rule" "webserver_sg_egress_all" {
   security_group_id = aws_security_group.webserver_sg.id
 
-  cidr_ipv4   = "0.0.0./0"
+  cidr_ipv4   = "0.0.0.0/0"
   from_port   = 0
   ip_protocol = "-1"
   to_port     = 0
