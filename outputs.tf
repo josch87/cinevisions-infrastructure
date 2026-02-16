e0306116e46f8f65f0660cce42bac270f320511f
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-16 15:57:06 +0100

    Align spacing in output blocks for consistent Terraform configuration formatting

--- outputs.tf
+++ outputs.tf
@@ -5,10 +5,10 @@ output "webserver_public_ip" {
 
 output "webserver_public_dns" {
   description = "Public DNS of the web server"
-  value = aws_instance.cinevisions_web_server.public_dns
+  value       = aws_instance.cinevisions_web_server.public_dns
 }
 
 output "vpc_id" {
   description = "The ID of the VPC"
-  value = aws_vpc.cinevisions_vpc.id
+  value       = aws_vpc.cinevisions_vpc.id
 }
