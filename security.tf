a2c07350b2a7fac039ee4fef1ff2b0d09bf03036
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-16 13:42:40 +0100

    Remove unused `from_port` and `to_port` fields in security group rule configuration.

--- security.tf
+++ security.tf
@@ -22,8 +22,6 @@ resource "aws_vpc_security_group_egress_rule" "webserver_sg_egress_all" {
   security_group_id = aws_security_group.webserver_sg.id
 
   cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 0
   ip_protocol = "-1"
-  to_port     = 0
 }
 
