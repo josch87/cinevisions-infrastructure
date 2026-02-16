8432f1a8e5d15655181e04605f07c95ba0e86e00
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-16 11:52:33 +0100

    Move security group resources from `main.tf` to `security.tf` for better Terraform configuration organization

--- main.tf
+++ main.tf
@@ -1,32 +1,3 @@
-resource "aws_security_group" "webserver_sg" {
-  name        = "webserver-sg"
-  description = "Security group for web server"
-  vpc_id      = aws_vpc.cinevisions_vpc.id
-
-  tags = {
-    Name        = "webserver-sg"
-    Environment = var.environment
-  }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "webserver_sg_ingress_http" {
-  security_group_id = aws_security_group.webserver_sg.id
-
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
-}
-
-resource "aws_vpc_security_group_egress_rule" "webserver_sg_egress_all" {
-  security_group_id = aws_security_group.webserver_sg.id
-
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 0
-  ip_protocol = "-1"
-  to_port     = 0
-}
-
 data "aws_ami" "amazon-linux_2023" {
   most_recent = true
   owners      = ["amazon"]
