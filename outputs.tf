824f65ac10a17ca74d18aeaa794085e5d7b16cfe
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-20 21:47:05 +0100

    Remove unused outputs for VPC ID and RDS endpoint.

--- outputs.tf
+++ outputs.tf
@@ -7,13 +7,3 @@ output "webserver_public_dns" {
   description = "Public DNS of the web server"
   value       = aws_instance.cinevisions_web_server.public_dns
 }
-
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.cinevisions_vpc.id
-}
-
-output "rds_endpoint" {
-  description = "The endpoint of the RDS instance"
-  value       = aws_db_instance.mariadb_rds.endpoint
-}
