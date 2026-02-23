80775de21a84380517d404122f466eb2a894d85b
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-23 18:15:32 +0100

    Add ingress and egress rules for ALB security group

--- security.tf
+++ security.tf
@@ -85,3 +85,19 @@ resource "aws_security_group" "alb" {
     Name = "${local.name_prefix}-alb-sg"
   }
 }
+
+resource "aws_vpc_security_group_ingress_rule" "alb_http" {
+  security_group_id = aws_security_group.alb.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_all" {
+  security_group_id = aws_security_group.alb.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
