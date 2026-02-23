5ab27a14423e13acf1b797c7a740bf5da90a7f05
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-23 13:59:13 +0100

    Add bastion host resource and corresponding output for public IP.

--- compute.tf
+++ compute.tf
@@ -16,6 +16,19 @@ data "aws_iam_instance_profile" "webserver" {
   name = var.iam_instance_profile_webserver
 }
 
+resource "aws_instance" "bastion_host" {
+  ami                    = data.aws_ami.amazon-linux_2023.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public_1.id
+  vpc_security_group_ids = [aws_security_group.ssh.id]
+  key_name               = var.key_name
+  iam_instance_profile   = data.aws_iam_instance_profile.webserver.name
+
+  tags = {
+    Name = "${local.name_prefix}-bastion-host"
+  }
+}
+
 resource "aws_instance" "webserver" {
   ami                    = data.aws_ami.amazon-linux_2023.id
   instance_type          = var.webserver_instance_type
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "webserver_public_dns" {
   description = "Public DNS of the web server"
   value       = aws_instance.webserver.public_dns
 }
+
+output "bastion_host_public_ip" {
+  description = "Public IP of the web server"
+  value       = aws_instance.bastion_host.public_ip
+}
