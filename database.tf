77ee04fecffb5e250b6fdf5531ff77f2a25eb872
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-23 11:59:43 +0100

    Clarify comment on `with_decryption` usage in SSM parameter declaration.

--- database.tf
+++ database.tf
@@ -5,7 +5,7 @@ data "aws_ssm_parameter" "rds_master_password" {
 
 data "aws_ssm_parameter" "wp_password" {
   name            = "/${var.project_name}/${var.environment}/db/wp_password"
-  with_decryption = false # Value not needed in state, only existence check
+  with_decryption = false # Secret value not needed in decrypted form; Terraform still stores the (encrypted) value in state and this is used only to assert the parameter exists
 }
 
 resource "aws_db_subnet_group" "database" {
