067ad966d3bf255cd88d245855b2fd6ed54d60da
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-24 16:03:24 +0100

    Rename SSH security group and associated resources to "bastion" for clarity and alignment with usage.

--- compute.tf
+++ compute.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "bastion_host" {
   ami                    = data.aws_ami.amazon-linux_2023.id
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public_1.id
-  vpc_security_group_ids = [aws_security_group.ssh.id]
+  vpc_security_group_ids = [aws_security_group.bastion.id]
   key_name               = var.key_name
   iam_instance_profile   = data.aws_iam_instance_profile.webserver.name
 
--- security.tf
+++ security.tf
@@ -24,18 +24,18 @@ resource "aws_vpc_security_group_egress_rule" "webserver_all" {
   ip_protocol = "-1"
 }
 
-resource "aws_security_group" "ssh" {
-  name        = "${local.name_prefix}-ssh-sg"
-  description = "SSH security group for developer access"
+resource "aws_security_group" "bastion" {
+  name        = "${local.name_prefix}-bastion-sg"
+  description = "Security group for bastion host (jump server)"
   vpc_id      = aws_vpc.main.id
 
   tags = {
-    Name = "${local.name_prefix}-ssh-sg"
+    Name = "${local.name_prefix}-bastion-sg"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ssh_ssh" {
-  security_group_id = aws_security_group.ssh.id
+resource "aws_vpc_security_group_ingress_rule" "bastion_ssh" {
+  security_group_id = aws_security_group.bastion.id
 
   cidr_ipv4   = var.my_local_public_ip
   from_port   = 22
@@ -43,8 +43,8 @@ resource "aws_vpc_security_group_ingress_rule" "ssh_ssh" {
   to_port     = 22
 }
 
-resource "aws_vpc_security_group_egress_rule" "ssh_all" {
-  security_group_id = aws_security_group.ssh.id
+resource "aws_vpc_security_group_egress_rule" "bastion_all" {
+  security_group_id = aws_security_group.bastion.id
 
   cidr_ipv4   = "0.0.0.0/0"
   ip_protocol = "-1"
