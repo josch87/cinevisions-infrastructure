1c75e87952a6002db8c40ef887bbf57e0e8c107c
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-24 16:03:40 +0100

    Add SSH ingress rule to webserver security group for bastion access

--- security.tf
+++ security.tf
@@ -17,6 +17,15 @@ resource "aws_vpc_security_group_ingress_rule" "webserver_http" {
   to_port                      = 80
 }
 
+resource "aws_vpc_security_group_ingress_rule" "webserver_ssh" {
+  security_group_id = aws_security_group.webserver.id
+
+  referenced_security_group_id = aws_security_group.bastion.id
+  from_port                    = 22
+  ip_protocol                  = "tcp"
+  to_port                      = 22
+}
+
 resource "aws_vpc_security_group_egress_rule" "webserver_all" {
   security_group_id = aws_security_group.webserver.id
 
