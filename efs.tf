475726267278df941b00754adabff1e644dd5dea
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-24 16:05:05 +0100

    Add EFS mount targets for private subnets

--- efs.tf
+++ efs.tf
@@ -5,3 +5,13 @@ resource "aws_efs_file_system" "wordpress_content" {
     Name = "${local.name_prefix}-wordpress-content-efs"
   }
 }
+
+resource "aws_efs_mount_target" "private_1" {
+  file_system_id = aws_efs_file_system.wordpress_content.id
+  subnet_id      = aws_subnet.private_1.id
+}
+
+resource "aws_efs_mount_target" "private_2" {
+  file_system_id = aws_efs_file_system.wordpress_content.id
+  subnet_id      = aws_subnet.private_2.id
+}
