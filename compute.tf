3df252fbd3ca2b1443aebd0ad4da830fc825efaa
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-24 14:13:24 +0100

    Remove webserver instance resource from `compute.tf`.

--- compute.tf
+++ compute.tf
@@ -28,25 +28,3 @@ resource "aws_instance" "bastion_host" {
     Name = "${local.name_prefix}-bastion-host"
   }
 }
-
-resource "aws_instance" "webserver" {
-  ami                    = data.aws_ami.amazon-linux_2023.id
-  instance_type          = var.webserver_instance_type
-  subnet_id              = aws_subnet.public_1.id
-  vpc_security_group_ids = [aws_security_group.webserver.id, aws_security_group.ssh.id]
-  key_name               = var.key_name
-  iam_instance_profile   = data.aws_iam_instance_profile.webserver.name
-  depends_on             = [aws_db_instance.mariadb, data.aws_ssm_parameter.wp_password]
-
-  user_data = templatefile("configure-webserver.sh.tftpl", {
-    project_name = var.project_name
-    environment  = var.environment
-    rds_host     = split(":", aws_db_instance.mariadb.endpoint)[0]
-    db_username  = aws_db_instance.mariadb.username
-  })
-  user_data_replace_on_change = var.environment == "dev"
-
-  tags = {
-    Name = "${local.name_prefix}-webserver"
-  }
-}
