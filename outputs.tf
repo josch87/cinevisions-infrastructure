9e9bbd961ff7ddd3b5e1d1a0b31bdf025395f40b
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-24 21:41:01 +0100

    Fix incorrect description for `bastion_host_public_ip` output in `outputs.tf`.

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,6 @@ output "alb_public_dns" {
 }
 
 output "bastion_host_public_ip" {
-  description = "Public IP of the web server"
+  description = "Public IP of the bastion host"
   value       = aws_instance.bastion_host.public_ip
 }
