a9c698738efc99314e3278b24d5070d321025d1b
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-24 14:40:38 +0100

    Standardize spacing in `security.tf` to improve readability.

--- security.tf
+++ security.tf
@@ -12,9 +12,9 @@ resource "aws_vpc_security_group_ingress_rule" "webserver_http" {
   security_group_id = aws_security_group.webserver.id
 
   referenced_security_group_id = aws_security_group.alb.id
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
+  from_port                    = 80
+  ip_protocol                  = "tcp"
+  to_port                      = 80
 }
 
 resource "aws_vpc_security_group_egress_rule" "webserver_all" {
